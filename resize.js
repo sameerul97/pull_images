e7a9cea76c246887e658443bfe8f0efc26213b86
smeerul97 <sameerul97@gmail.com>
2019-12-18 21:34:49 +0000

    Code refactoring

--- resize.js
+++ resize.js
@@ -7,20 +7,12 @@ fs.readdirSync(imageFolder).forEach(file => {
     console.log(file);
     if (!fs.existsSync(blurryFolder)) {
         fs.mkdirSync(blurryFolder);
-        sharp("./images/" + file)
-            .resize(780, null)
-            .jpeg({
-                quality: 50,
-                chromaSubsampling: '4:4:4'
-            })
-            .toFile(blurryFolder + file, function(err) {});
-    } else {
-        sharp("./images/" + file)
-            .resize(780, null)
-            .jpeg({
-                quality: 50,
-                chromaSubsampling: '4:4:4'
-            })
-            .toFile(blurryFolder + file, function(err) {});
     }
+    sharp("./images/" + file)
+        .resize(780, null)
+        .jpeg({
+            quality: 50,
+            chromaSubsampling: '4:4:4'
+        })
+        .toFile(blurryFolder + file, function(err) {});
 });
